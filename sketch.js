65a7f6b76f2e38d7eccdae256cf7f244fa77782e
Cuong Le <cle15@lsu.edu>
2016-11-08 10:59:32 -0600

    needs work

--- sketch.js
+++ sketch.js
@@ -1,37 +1,47 @@
-var cities;
+
+
+var trafficInc;
 var rowCount;
 
 function preload(){
-
-  cities =loadTable("media/cities.csv", "header");
+  trafficInc =loadTable("2014_Traffic_Incidents.csv", "header");
 }
 
 function setup(){
 
-createCanvas(1024, 764);
+createCanvas(640, 480);
 
-rowCount = cities.getRowCount();
-background(0);
-fill(255);
+rowCount = trafficInc.getRowCount()-15000;
+println(rowCount);
+
+// background(0);
+fill(255,150);
 noStroke();
 
 //background(map(rowCount, 0, 30000, 0, 200));
-
 }
 function draw(){
-translate(width/2 - 470, height/2- 320);
-rotate(radians(270));
+  // background(0);
+//  scale(2.0);
+
+// translate(width/2 - 120, height/2+320);
+// rotate(radians(270));
+ scale(12);
+
   for (var i = 0; i <rowCount; i++){
-    var latitude = cities.getNum(i,"lat");
-    var longitude = cities.getNum(i,"long");
-    //console.log(latitude, longitude);
-    setXY(latitude,longitude);
+    var district = trafficInc.getNum(i,"DISTRICT");
+    // var subzone = trafficInc.getNum(i,"SUBZONE");
+
+    println(district);
+    setXY(i, district);
 }
 }
 
-function setXY(lat,long){
-  var x = map(lat, -90, 90, 0, height);
-  var y = map(long, -180, 180, 0, width);
-  ellipse(x,y,0.25);
+function setXY(n, dist){
+  // var x = map(lat, -90, 90, 0, width);
+  // var y = map(long, -180, 180, 0, height);
+  fill(10, 100);
+  ellipse(map(dist, 1, 4, 10, 50),height/2,10);
+
 
 }
